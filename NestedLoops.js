db477e53c8f0668189ba003ba4804121f2b8106c
Orozco Oscar <88407386+OrozcoOscar@users.noreply.github.com>
2022-11-19 10:03:54 -0500

    Update NestedLoops.js

--- NestedLoops.js
+++ NestedLoops.js
@@ -1,23 +1,31 @@
-function NestedLoops(f=(e)=>{console.log(...e)},nFores=1,i=0,n=5,inc=1,it=nFores-1,v=[]){
-            if(v.length==0){
-                v=(new Array(nFores)).fill(i)//rellenamos con i
-                f(v)
-            }
-            if(v.reduce((a,v)=>a+v)/nFores==n-1|| inc>=n || it<0){
-                //si el promedio es igual a la cantidad total de iteraciones-1
-                // si el incremento es mayor o igual a n
-                //si el iterador esta fuera del vector 
-                return;
-            }
-            if(v[it]+inc<n){
-                v[it]+=inc
-                if(it+1<nFores){
-                    it=nFores-1
-                }
-                f(v)   
-            }else{
-                v[it]=i
-                it-=1
-            }
-            NestedLoops(f,nFores,i,n,inc,it,v)
+/**
+ * Simula Ciclos anidados
+ * @param {Function} f recibe una función la cual espera como parámetro los iteradores de los ciclos 
+ * @param {Number} nLoops numero de ciclos a generar
+ * @param {Number} i valor inicial de cada ciclo 
+ * @param {Number} n valor con el q terminan Ej: en un for (let i=0;i<n:i++)
+ * @param {Number} inc incremento en un for seria i++ o i+=1
+ */
+function NestedLoops(f=(e)=>{console.log(...e)},nLoops=1,i=0,n=5,inc=1,it=nLoops-1,v=[]){
+    if(v.length==0){
+        v=(new Array(nLoops)).fill(i)//rellenamos con i
+        f(v)
+    }
+    if(v.reduce((a,v)=>a+v)/nLoops==n-1|| inc>=n || it<0){
+        //si el promedio es igual a la cantidad total de iteraciones-1
+        // si el incremento es mayor o igual a n
+        //si el iterador esta fuera del vector 
+        return;
+    }
+    if(v[it]+inc<n){
+        v[it]+=inc
+        if(it+1<nLoops){
+            it=nLoops-1
         }
+        f(v)   
+    }else{
+        v[it]=i
+        it-=1
+    }
+    NestedLoops(f,nLoops,i,n,inc,it,v)
+}
